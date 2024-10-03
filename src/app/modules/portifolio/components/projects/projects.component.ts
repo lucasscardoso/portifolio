1d9e1a8cb29b0f953a33869c79c61a0cd6631846
lucasscardoso <gajeel.red.fox@hotmai.com>
2024-10-03 15:27:53 -0300

    corrigido alguns erros em experience e header scss, criado component dialog, criado funcao para abrir o dialog utilziando algular material

--- src/app/modules/portifolio/components/projects/projects.component.ts
+++ src/app/modules/portifolio/components/projects/projects.component.ts
@@ -1,16 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+
+//material
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 
 // interface
 import { IProjects } from '../../interface/IProjects.interface';
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+//dialog
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+
+  #dialog = inject(MatDialog);
 
   public arrayProjects = signal<IProjects[]>([
     {
@@ -28,7 +36,46 @@ export class ProjectsComponent {
       ]
 
 
+    },
+    {
+      src: 'assets/img/projects/playflix.png',
+      alt: "Projeto Lista de Tarefas",
+      title: "Lista de Tarefas",
+      width: '100px',
+      height: '51px',
+      description: '',
+      links: [
+          {
+            name: 'GitHub Pages',
+            href: 'https://lucasscardoso.github.io/To-do-List/',
+          }
+      ]
+
+
+    },
+    {
+      src: 'assets/img/projects/pokedex.png',
+      alt: "Projeto Lista de Tarefas",
+      title: "Lista de Tarefas",
+      width: '100px',
+      height: '51px',
+      description: '',
+      links: [
+          {
+            name: 'GitHub Pages',
+            href: 'https://lucasscardoso.github.io/To-do-List/',
+          }
+      ]
+
+
     }
-])
+]);
+
+public openDialog(data:IProjects){
+    this.#dialog.open(DialogProjectsComponent,{
+      data,
+      panelClass: EDialogPanelClass.PROJECTS
+    })
+}
 
 }
