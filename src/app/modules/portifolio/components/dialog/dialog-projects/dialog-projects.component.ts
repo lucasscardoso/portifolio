933a4934dbf809f56347f0d467198eb63e94b17e
lucasscardoso <gajeel.red.fox@hotmai.com>
2024-10-04 10:43:30 -0300

    adicionado scss a pagina dialog, foi criado um enum para dialog. finalizado o projeto com sua atualização, foi adicionado um novo botão git hub

--- src/app/modules/portifolio/components/dialog/dialog-projects/dialog-projects.component.ts
+++ src/app/modules/portifolio/components/dialog/dialog-projects/dialog-projects.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, signal } from '@angular/core';
+
+// material
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+
+// interface
+import { IProjects } from '../../../interface/IProjects.interface';
+
 
 @Component({
   selector: 'app-dialog-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './dialog-projects.component.html',
   styleUrl: './dialog-projects.component.scss'
 })
-export class DialogProjectsComponent {
+export class DialogProjectsComponent implements OnInit{
+
+  constructor(
+    private _dialogRef: MatDialogRef<DialogProjectsComponent>,
+    @Inject(MAT_DIALOG_DATA) private _data: IProjects){}
+
+  public getData = signal<IProjects | null>(null);
+
+  ngOnInit(): void {
+    this.getData.set(this._data);
+  }
+
+  public closeModal(){
+    this._dialogRef.close();
+  }
+
 
 }
--- src/app/modules/portifolio/components/projects/projects.component.ts
+++ src/app/modules/portifolio/components/projects/projects.component.ts
@@ -27,10 +27,10 @@ export class ProjectsComponent {
       title: "Lista de Tarefas",
       width: '100px',
       height: '51px',
-      description: '',
+      description: 'O projeto "To-Do List" é uma aplicação web simples que permite aos usuários gerenciar tarefas. Você pode adicionar, marcar como concluída e remover tarefas da lista. A interface é minimalista e fácil de usar, proporcionando uma experiência intuitiva. O design é responsivo, o que garante uma boa usabilidade em diferentes dispositivos.',
       links: [
           {
-            name: 'GitHub Pages',
+            name: 'Visite em GitHub Pages',
             href: 'https://lucasscardoso.github.io/To-do-List/',
           }
       ]
@@ -40,14 +40,14 @@ export class ProjectsComponent {
     {
       src: 'assets/img/projects/playflix.png',
       alt: "Projeto Lista de Tarefas",
-      title: "Lista de Tarefas",
+      title: "PlayFlix",
       width: '100px',
       height: '51px',
-      description: '',
+      description: 'O projeto "PlayFlix" é uma plataforma de streaming fictícia , foi desenvolvido em parceria com outros colaboradores como um projeto de um curso. Onde Trabalhei nas paginas Home,perfil e Filmes/series/animes, tambem realizei trabalhos com css e javascript. Semelhante a serviços de vídeo sob demanda. O site oferece uma interface que exibe lançamentos de filmes e séries, com opções para login e cadastro de usuários. Ele simula uma experiência de navegação em catálogos de mídia digital. Além disso, apresenta informações de contato, como telefone e e-mail.',
       links: [
           {
             name: 'GitHub Pages',
-            href: 'https://lucasscardoso.github.io/To-do-List/',
+            href: 'https://lucasscardoso.github.io/projeto_integrador/',
           }
       ]
 
@@ -55,15 +55,15 @@ export class ProjectsComponent {
     },
     {
       src: 'assets/img/projects/pokedex.png',
-      alt: "Projeto Lista de Tarefas",
-      title: "Lista de Tarefas",
+      alt: "Projeto Pokedex",
+      title: "Pokedex",
       width: '100px',
       height: '51px',
-      description: '',
+      description: 'O projeto "PokedexAngular" é uma aplicação desenvolvida com Angular CLI para criar uma Pokédex interativa. Ele permite a navegação por diferentes componentes, oferecendo uma experiência de visualização e busca de informações sobre Pokémon. O projeto está estruturado para facilitar o desenvolvimento e a construção de novas funcionalidades, com suporte para testes unitários e testes end-to-end.',
       links: [
           {
-            name: 'GitHub Pages',
-            href: 'https://lucasscardoso.github.io/To-do-List/',
+            name: 'Visite em GitHub Pages',
+            href: 'https://lucasscardoso.github.io/Pokedex/',
           }
       ]
 
